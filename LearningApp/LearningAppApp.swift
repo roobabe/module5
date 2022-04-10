5e12458f5f634b5c910d383dda2988bb3322b17d
Kynda Montero-Langston <roobabe@rocketmail.com>
2022-04-10 16:47:54 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Kynda Montero-Langston on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

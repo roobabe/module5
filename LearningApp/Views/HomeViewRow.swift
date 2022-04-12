87461661261283e943ea579f557bd81e72bffda2
Kynda Montero-Langston <roobabe@rocketmail.com>
2022-04-12 13:23:41 -0500

    - add HomeRowView.swift subview - update HomeView.swift with subview

---
+++ LearningApp/Views/HomeViewRow.swift
@@ -0,0 +1,72 @@
+//
+//  HomeViewRow.swift
+//  LearningApp
+//
+//  Created by Kynda Montero-Langston on 4/12/22.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    
+    var image: String
+    var title: String
+    var description: String
+    var count: String
+    var time: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(color: .gray, radius: 10, x: 5, y: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            HStack {
+                //Image
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                Spacer()
+                
+                //Text
+                VStack (alignment: .leading, spacing: 10) {
+                    //Headline
+                    Text("Learn \(title)")
+                        .bold()
+                    //Description
+                    Text(description)
+                        .padding(.bottom, 20)
+                        .font(.caption)
+                    //Icons
+                    HStack {
+                        //Number of lessons/questions
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(count)
+                            .font(Font.system(size: 10))
+                        Spacer()
+                        //Time
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(.caption)
+                            .font(Font.system(size: 10))
+                    }
+                }
+                .padding(.leading, 20)
+            }
+            .padding(.horizontal, 20)
+        }
+        
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "Learn Swift", description: "some description", count: "10 Lessons", time: "2 Hours")
+    }
+}

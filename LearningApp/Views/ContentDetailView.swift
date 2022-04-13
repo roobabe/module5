63f1b1983f838d440cf2f42d9db569172fd30b62
Kynda Montero-Langston <roobabe@rocketmail.com>
2022-04-12 19:18:18 -0500

    - adjust video player size

--- LearningApp/Views/ContentDetailView.swift
+++ LearningApp/Views/ContentDetailView.swift
@@ -11,40 +11,45 @@ import AVKit
 struct ContentDetailView: View {
     
     @EnvironmentObject var model: ContentModel
+    //Learn Swift for Beginner video is 910X511
+    let videoRatio: CGFloat = 511/910
     
     var body: some View {
         
         let lesson = model.currentLesson
         let url = URL(string: Constants.videoHostURL + (lesson?.video ?? ""))
-     
-        VStack {
-            //Only show video if we get a valid URL
-            if url != nil {
-                VideoPlayer(player: AVPlayer(url: url!))
-                    .cornerRadius(10)
-            }
-            //TODO: Description
-            
-            //Next Lesson button
-            if model.hasNextLesson() {
-                Button(action: {
-                    //Advance the lesson
-                    model.nextLesson()
-                }, label: {
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(.green)
-                            .cornerRadius(10)
-                            .shadow(color: .gray, radius: 5, x: 5, y: 5)
-                            .frame(height: 48)
-                        Text("Next Lesson: \(model.currentModule!.content.lessons[model.currentLessonIndex + 1].title)")
-                            .foregroundColor(.white)
-                            .bold()
-                    }
-            })
-    }
+        GeometryReader {geo in
+            VStack {
+                //Only show video if we get a valid URL
+                if url != nil {
+                    VideoPlayer(player: AVPlayer(url: url!))
+                        .cornerRadius(10)
+                        .frame(height: geo.size.width * videoRatio)
+                }
+                //TODO: Description
+                
+                //Next Lesson button
+                if model.hasNextLesson() {
+                    Button(action: {
+                        //Advance the lesson
+                        model.nextLesson()
+                    }, label: {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.green)
+                                .cornerRadius(10)
+                                .shadow(color: .gray, radius: 5, x: 5, y: 5)
+                                .frame(height: 48)
+                            Text("Next Lesson: \(model.currentModule!.content.lessons[model.currentLessonIndex + 1].title)")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                })
         }
-        .padding()
+            }
+            .padding()
+
+        }
     }
 }
 

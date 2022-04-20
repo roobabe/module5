1e4b9afd71ce0420f60da236bf29f32b85dc11ae
Kynda Montero-Langston <roobabe@rocketmail.com>
2022-04-19 21:41:57 -0500

    - nextQuestion method in the contentModel - dynamic button text in testView - advance to the next question in testView

--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -78,16 +78,30 @@ struct TestView: View {
                 
                 // Submit Button
                 Button {
-                    // change submitted state to true
-                    submitted = true
-                    //Check the answer and increment the counter if correct
-                    
+                    //check if answer has been submitted
+                    if submitted == true {
+                        //answer has already been submitted, move to next question
+                        model.nextQuestion()
+                        
+                        //reset properties
+                        submitted = false
+                        selectedAnswerIndex = nil
+                    }
+                    else {
+                        // change submitted state to true
+                        submitted = true
+                        
+                        //Check the answer and increment the counter if correct
+                        if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                            numCorrect += 1
+                        }
+                    }
                 } label: {
                     ZStack {
                         RectangleCard(color: .green)
                             .frame(height: 48)
                         
-                        Text("Submit")
+                        Text(buttonText)
                             .bold()
                             .foregroundColor(.white)
                     }
@@ -103,6 +117,22 @@ struct TestView: View {
             ProgressView()
         }
         
+    }
+    
+    var buttonText:String {
+        //Check if answer has been submitted
+        if submitted == true {
+            if model.currentQuestionIndex + 1 == model.currentModule!.test.questions.count {
+                //this is the last question
+                return "Finish"
+            }
+            else {
+                return "Next" // or finish
+            }
+        }
+        else {
+            return "Submit"
+        }
     }
 }
 

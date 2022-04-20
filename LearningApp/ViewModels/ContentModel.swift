200218a6441af6055d3671d1539f3cb9b7463428
Kynda Montero-Langston <roobabe@rocketmail.com>
2022-04-19 20:38:11 -0500

    - rename lessonDescription property in content model to codeText - add TestView - add NavigationLink in the HomeView for tests - add properties in ContentModel to track Test selected - add method beginTest in ContentModel

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -2,84 +2,105 @@
 //  ContentModel.swift
 //  LearningApp
 //
-//  Created by Kynda Montero-Langston on 4/11/22.
+//  April 19, 2022 M5L9
 //
 
 import Foundation
 
 class ContentModel: ObservableObject {
     
+    // List of modules
     @Published var modules = [Module]()
     
     // Current module
     @Published var currentModule: Module?
     var currentModuleIndex = 0
     
-    //current lesson
+    // Current lesson
     @Published var currentLesson: Lesson?
     var currentLessonIndex = 0
     
-    //Current lesson explanation
-    @Published var lessonDescription = NSAttributedString()
+    // Current question
+    @Published var currentQuestion: Question?
+    var currentQuestionIndex = 0
+    
+    // Current lesson explanation
+    @Published var codeText = NSAttributedString()
     var styleData: Data?
     
-    //Current selected content and test
+    // Current selected content and test
     @Published var currentContentSelected:Int?
+    @Published var currentTestSelected:Int?
+    
     
     init() {
+        
         getLocalData()
+        
     }
     
-    //MARK: - Data Methods
+    // MARK: - Data methods
+    
     func getLocalData() {
-        //Get a url to the jason file
+        
+        // Get a url to the json file
         let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
         do {
-            //Read the file into a data object
+            // Read the file into a data object
             let jsonData = try Data(contentsOf: jsonUrl!)
-            //Try to decode the json into array of modules
+            
+            // Try to decode the json into an array of modules
             let jsonDecoder = JSONDecoder()
             let modules = try jsonDecoder.decode([Module].self, from: jsonData)
-            //Assign parsed modules to modules property
+            
+            // Assign parsed modules to modules property
             self.modules = modules
         }
         catch {
-            //TODO log error
+            // TODO log error
             print("Couldn't parse local data")
         }
         
-        //Parse the style data
+        // Parse the style data
         let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
         
         do {
-            //Read the file into a data object
+            
+            // Read the file into a data object
             let styleData = try Data(contentsOf: styleUrl!)
             
             self.styleData = styleData
         }
         catch {
-            //log error
+            // Log error
             print("Couldn't parse style data")
         }
+        
     }
     
-    //MARK: - Module navigation methods
+    // MARK: - Module navigation methods
+    
     func beginModule(_ moduleid:Int) {
-        //Find the index for this module id
+        
+        // Find the index for this module id
         for index in 0..<modules.count {
+            
             if modules[index].id == moduleid {
-                //Found matching module
+            
+                // Found the matching module
                 currentModuleIndex = index
                 break
             }
         }
-        //Set the current module
-        currentModule = modules[currentModuleIndex]
         
+        // Set the current module
+        currentModule = modules[currentModuleIndex]
     }
     
     func beginLesson(_ lessonIndex:Int) {
-        //Find the index for this lesson
+        
+        // Check that the lesson index is within range of module lessons
         if lessonIndex < currentModule!.content.lessons.count {
             currentLessonIndex = lessonIndex
         }
@@ -87,62 +108,73 @@ class ContentModel: ObservableObject {
             currentLessonIndex = 0
         }
         
-        //Set the current lesson
+        // Set the current lesson
         currentLesson = currentModule!.content.lessons[currentLessonIndex]
-        lessonDescription = addStyling(currentLesson!.explanation)
+        codeText = addStyling(currentLesson!.explanation)
     }
     
     func nextLesson() {
-        //Advance the lesson
+        
+        // Advance the lesson index
         currentLessonIndex += 1
         
-        //Check that it is within range
+        // Check that it is within range
         if currentLessonIndex < currentModule!.content.lessons.count {
-            //Set the current lesson property
+            
+            // Set the current lesson property
             currentLesson = currentModule!.content.lessons[currentLessonIndex]
-            lessonDescription = addStyling(currentLesson!.explanation)
+            codeText = addStyling(currentLesson!.explanation)
         }
         else {
-            //Reset the lesson state
+            // Reset the lesson state
             currentLessonIndex = 0
             currentLesson = nil
         }
     }
     
     func hasNextLesson() -> Bool {
+        
         return (currentLessonIndex + 1 < currentModule!.content.lessons.count)
     }
     
-    //MARK: Code Styling
+    func beginTest(_ moduleId:Int) {
+        
+        // Set the current module
+        beginModule(moduleId)
+        
+        // Set the current question index
+        currentQuestionIndex = 0
+        
+        // If there are questions, set the current question to the first one
+        if currentModule?.test.questions.count ?? 0  > 0 {
+            currentQuestion = currentModule!.test.questions[currentQuestionIndex]
+            
+            // Set the question content
+            codeText = addStyling(currentQuestion!.content)
+        }
+    }
+    
+    // MARK: - Code Styling
+    
     private func addStyling(_ htmlString: String) -> NSAttributedString {
-        var resultsString = NSAttributedString()
+        
+        var resultString = NSAttributedString()
         var data = Data()
         
-        //Add the styling data
+        // Add the styling data
         if styleData != nil {
             data.append(styleData!)
         }
-        //add the html data
+        
+        // Add the html data
         data.append(Data(htmlString.utf8))
-        //Convert to attributed string
-        //Technique 1 - doesn't handle error
+        
+        // Convert to attributed string
         if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
-            resultsString = attributedString
+            
+            resultString = attributedString
         }
         
-        /*
-         Technique 2
-         
-         do {
-            let attributedString = try NSAttributedString(data: data, options: [.documentType:
-                NSAttributedString.DocumentType.html], documenAttributes: nil)
-            resultString = attributedString
-         }
-         catch {
-            print("Couldn't turn html into attributed string")
-         }
-         */
-        
-        return resultsString
+        return resultString
     }
 }
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -27,14 +27,20 @@ struct HomeView: View {
                                         .onAppear(perform: { model.beginModule(module.id)
                                         }),
                                     tag: module.id,
-                                    selection: $model.currentContentSelected,
-                                    label: {
+                                    selection: $model.currentContentSelected) {
                                         //Learning Card
                                         HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
-                                    })
-                                
-                                //Test Card
-                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Questions", time: module.test.time)
+                                    }
+                                NavigationLink(
+                                    destination:
+                                        TestView()
+                                        .onAppear(perform: { model.beginTest(module.id)
+                                        }),
+                                    tag: module.id,
+                                    selection: $model.currentTestSelected) {
+                                        //Test Card
+                                        HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Questions", time: module.test.time)
+                                    }
                             }
                         }
                     }
